06bc941264a2201d9077646ab52a8f671c325256
/\be| /\nand <90404118+AbelXAnand13@users.noreply.github.com>
2021-10-26 10:52:31 -0700

    Initial Commit

---
+++ scienceQuiz/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  scienceQuiz
+//
+//  Created by Abel Christy Anand on 10/26/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
